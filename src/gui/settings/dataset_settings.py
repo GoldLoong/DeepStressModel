f6507c16c108866fdcede656009dbe8dfe1c1e72
GoldLoong <443423777@qq.com>
2025-05-18 17:08:26 +0800

    Update dataset_settings.py

--- src/gui/settings/dataset_settings.py
+++ src/gui/settings/dataset_settings.py
@@ -145,7 +145,7 @@ class DatasetSettingsWidget(QWidget):
         self.details_label = QLabel()
         self.details_label.setWordWrap(True)
         self.details_label.setTextFormat(Qt.TextFormat.RichText)
-        self.details_label.setStyleSheet("QLabel { background-color: #f0f0f0; padding: 5px; border-radius: 3px; }")
+        self.details_label.setStyleSheet("QLabel { background-color: #707070; padding: 5px; border-radius: 3px; }")
         group_layout.addWidget(self.details_label)
         
         self.dataset_group.setLayout(group_layout)
